bd152666247bd461faa7c4ee4c88e10b108c3cfa
anod <alex.gavrishev@gmail.com>
2012-06-16 11:39:33 +0300

    update new cab dialog

--- src/com/station/taxi/gui/NewCabDialog.java
+++ src/com/station/taxi/gui/NewCabDialog.java
@@ -8,6 +8,12 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import java.awt.FlowLayout;
+import javax.swing.BoxLayout;
+import javax.swing.border.EmptyBorder;
 
 public class NewCabDialog extends JDialog {
 	/**
@@ -17,8 +23,17 @@ public class NewCabDialog extends JDialog {
 	private JTextField mNumber;
 
 	public NewCabDialog(Frame owner) {
+		setTitle(TextsBundle.getString("dialog_addcab_title")); //$NON-NLS-1$
 		new JDialog(owner, TextsBundle.getString("dialog_title_addcab"));
+		setupViews();
+		pack();
+		setLocationRelativeTo(null);		
+	}
 
+	/**
+	 * 
+	 */
+	private void setupViews() {
 		JPanel btnPanel = new JPanel();
 		JButton okBtn = new JButton(TextsBundle.getString("dialog_add"));
 		JButton noBtn = new JButton(TextsBundle.getString("dialog_cancel"));
@@ -35,18 +50,37 @@ public class NewCabDialog extends JDialog {
 			}
 		});
 		btnPanel.add(noBtn);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(panel, BorderLayout.WEST);
+		panel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JLabel lblNewLabel_1 = new JLabel(TextsBundle.getString("dialog_addcab_num")); //$NON-NLS-1$
+		panel.add(lblNewLabel_1);
 		mNumber = new JTextField(32);
-		getContentPane().add(mNumber, BorderLayout.NORTH);
+		panel.add(mNumber);
+		
+		JLabel lblNewLabel = new JLabel(TextsBundle.getString("dialog_addcab_waiting")); //$NON-NLS-1$
+		panel.add(lblNewLabel);
+		
+		String[] waitingStrings = { 
+			TextsBundle.getString("cab_waiting_eat"),
+			TextsBundle.getString("cab_waiting_drink"),
+			TextsBundle.getString("cab_waiting_read_news_papper")
+		};
+
+		JComboBox comboBox = new JComboBox(waitingStrings);
+		panel.add(comboBox);
 		getContentPane().add(btnPanel, BorderLayout.SOUTH);
-		pack();
 	}
 	
 	private void okButton() {   
-	      setVisible(false);  
-	   }  
+		setVisible(false);  
+	}
 
 	private void noButton() {  
-	      setVisible(false);  
+		setVisible(false);  
 	}
 
 }
