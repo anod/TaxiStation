b4d7e22bdaf6cd2187d2c37d3c057c37b58f26ff
anod <alex.gavrishev@gmail.com>
2012-06-30 22:45:51 +0300

    Move station window adapter into separate class and initialize station when window is open

--- src/com/station/taxi/TaxiStationMain.java
+++ src/com/station/taxi/TaxiStationMain.java
@@ -1,80 +1,27 @@
 package com.station.taxi;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
 import javax.swing.SwingUtilities;
 
-import com.station.taxi.configuration.StationConfigLoader;
-import com.station.taxi.configuration.StationConfigStorage;
 import com.station.taxi.gui.StationFrame;
+import com.station.taxi.gui.StationWindowAdapter;
 /**
- * Main entry point for the graphical taxiStation program
+ * Main entry point
  * @author alex
  * @author Eran Zimbler
  * @version 0.2
  */
 public class TaxiStationMain {
-	
-    private static final String CONFIG_XML = "configs/config1.xml";
 
 	public static void main(String[] args) {
-    	//Create configuration loader
-    	StationConfigLoader configLoader = new StationConfigLoader(CONFIG_XML);
-
-    	final Station station;
-    	try {
-    		//Load station from configuration
-			station = configLoader.load();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return;
-		}
-
-    	SwingUtilities.invokeLater(new Runnable() {
+		SwingUtilities.invokeLater(new Runnable() {
 			
 			@Override
 			public void run() {
 				StationFrame window = new StationFrame();
-				station.registerStateListener(window);
-		    	window.addWindowListener(new StationWindowAdapter(station));
+				window.addWindowListener(new StationWindowAdapter(window));
 			}
 		});
-    	
-    }
-    
-    private static class StationWindowAdapter extends WindowAdapter {
-    	private Station mStation;
-    	
-    	/* (non-Javadoc)
-		 * @see java.awt.event.WindowAdapter#windowClosing(java.awt.event.WindowEvent)
-		 */
-		public StationWindowAdapter(Station station) {
-			super();
-			mStation = station;
-		}
 
-		@Override
-		public void windowClosing(WindowEvent e) {
-			//Stop the thread
-			mStation.interrupt();
-			//mStation configuration
-			StationConfigStorage configStorage = new StationConfigStorage(CONFIG_XML);
-			configStorage.save(mStation);
-			super.windowClosing(e);				
-		}
+	}
 
-		/* (non-Javadoc)
-		 * @see java.awt.event.WindowAdapter#windowOpened(java.awt.event.WindowEvent)
-		 */
-		@Override
-		public void windowOpened(WindowEvent e) {
-			super.windowOpened(e);
-			//Start station thread
-			mStation.start();
-		}
-		
-    }
-    
-    
 }
