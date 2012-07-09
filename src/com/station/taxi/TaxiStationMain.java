7c341cc2581e7b9a394d2642113adf2ad90a0eae
anod <alex.gavrishev@gmail.com>
2012-07-09 20:45:57 +0300

    add configuration file + spring-javadoc archive

--- src/com/station/taxi/TaxiStationMain.java
+++ src/com/station/taxi/TaxiStationMain.java
@@ -3,6 +3,8 @@ package com.station.taxi;
 import com.station.taxi.gui.StationFrame;
 import com.station.taxi.gui.StationWindowAdapter;
 import javax.swing.SwingUtilities;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * Main entry point
  * @author alex
@@ -10,8 +12,12 @@ import javax.swing.SwingUtilities;
  * @version 0.2
  */
 public class TaxiStationMain {
+    private static final String CONFIG_PATH = "com/station/taxi/spring/StationXMLConfig.xml";
 
 	public static void main(String[] args) {
+            
+            ApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_PATH);
+            
 		SwingUtilities.invokeLater(new Runnable() {
 			
 			@Override
