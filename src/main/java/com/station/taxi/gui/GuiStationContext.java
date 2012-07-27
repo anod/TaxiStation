706c6bef856aa9eb8599f7dcfa994062767ec33e
anod <alex.gavrishev@gmail.com>
2012-07-27 14:24:45 +0300

    Remove generic comment in titles of classes

--- src/main/java/com/station/taxi/gui/GuiStationContext.java
+++ src/main/java/com/station/taxi/gui/GuiStationContext.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.station.taxi.gui;
 
 import com.station.taxi.spring.StationContext;
--- src/main/java/com/station/taxi/spring/StationContext.java
+++ src/main/java/com/station/taxi/spring/StationContext.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.station.taxi.spring;
 
 import com.station.taxi.model.Cab;
