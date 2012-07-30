550284a31ff853fe513a8a6ee705a4ab9edc579b
anod <>
2012-07-30 20:41:39 +0300

    add test db main back

---
+++ src/main/java/com/station/taxi/db/JPATestMain.java
@@ -0,0 +1,59 @@
+package com.station.taxi.db;
+
+import com.station.taxi.db.repositories.ReceiptRepository;
+import com.station.taxi.gui.GuiMain;
+import com.station.taxi.model.Receipt;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author alex
+ */
+public class JPATestMain {
+
+	private static final String CONFIG_PATH = "GuiXMLConfig.xml";
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(CONFIG_PATH, GuiMain.class);
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("TaxiStationPU");
+		EntityManager em = emf.createEntityManager();
+		Receipt r1 = em.find(Receipt.class, 1L);
+		System.out.println(r1);
+		
+		ReceiptRepository repository = applicationContext.getBean(ReceiptRepository.class);
+
+		Receipt newReceipt1 = new Receipt(new Date(), new Date(), 16.5f, 2);
+		Receipt newReceipt2 = new Receipt(new Date(), new Date(), 15.5f, 1);
+		Receipt newReceipt3 = new Receipt(new Date(), new Date(), 17.5f, 3);
+
+	//	repository.save(newReceipt1);
+//		repository.save(newReceipt2);
+//		repository.save(newReceipt3);
+	
+			System.out.println("====-11111-=====");
+			
+		Iterable<Receipt> all = repository.findAll();
+		for(Receipt r: all) {
+			System.out.println(r);
+		}
+
+			System.out.println("====-22222-====");
+		
+		all = repository.findByPassengerNumber(2);
+		for(Receipt r: all) {
+			System.out.println(r);
+		}
+		
+		
+	}
+}
